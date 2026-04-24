a2526191e1df29038b0a5b50cb320bd43803a566
chinoBasoalto <ezebasoalto@gmail.com>
2026-04-24 00:14:24 +0000

    Renombro ej5 a app.js para c04

---
+++ c03-javascript/ej5/app.js
@@ -0,0 +1,32 @@
+const boton = document.getElementById("generarBtn");
+const input = document.getElementById("alturaInput");
+const resultado = document.getElementById("resultado");
+
+boton.addEventListener("click", () => {
+   const altura = parseInt(input.value);
+
+   //Validacion
+   if (isNaN (altura) || altura <= 1) { 
+      resultado.innerText = "Error: Por favor, ingrese un número entero mayor a 1.";
+      return;
+
+   }
+
+   //Generar el medio árbol
+    let arbol = "";
+    for (let i = 1; i <= altura; i++) {
+        arbol += generarAsteriscos(i) + "\n";
+    }
+
+    resultado.innerText = arbol;
+
+});
+
+//Reutilizo la lógica del ejercicio anterior para generar asteriscos
+function generarAstericos(n) {
+   let linea = "";
+   for (let i = 0; i < n; i++) {
+      linea += "*";
+   }
+   return linea;
+}
