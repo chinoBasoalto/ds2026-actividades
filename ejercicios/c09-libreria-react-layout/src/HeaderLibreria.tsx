49917a8edb6a1d0da14dc56e43621869929652c2
chinoBasoalto <ezebasoalto@gmail.com>
2026-06-07 19:23:47 +0000

    refactor: estructura de carpetas.

---
+++ ejercicios/c09-libreria-react-layout/src/HeaderLibreria.tsx
@@ -0,0 +1,28 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { BookHalf } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+
+function HeaderLibreria() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" className="mb-4 shadow">
+            <Container>
+                {/* Cambiado a Link de React Router */}
+                <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2 fw-bold fs-3 text-warning">
+                    <BookHalf /> Ateneo React
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto fs-5">
+                        {/* Enlaces de navegación con 'as={Link} to="..."' */}
+                        <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+                        <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default HeaderLibreria;
